9b07cc2afa0cae736ecbd054b7fce5fb124fee19
gangsaryp <gangsaryoga1996@gmail.com>
2021-06-09 07:37:41 +0700

    commit

---
+++ database/migrations/2021_06_08_130919_anggota.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Anggota extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anggota', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',100);
+            $table->text('address');
+            $table->integer('phone_number');
+            $table->string('username',100);
+            $table->string('password',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anggota');
+    }
+}
